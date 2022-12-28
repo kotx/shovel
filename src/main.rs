85a43e43db7e37c2af2b595a201b06baeead1eae
Kot <kot@yukata.tech>
2022-12-28 00:24:19 +0000

    Don't use rayon yet, it's inefficient

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
 mod icmp;
 mod place;
 
-use std::time::Duration;
-
 use clap::Parser;
 use icmp::ping_v6;
 use image::{imageops::FilterType, GenericImageView};
-use rayon::prelude::*;
 
-use crate::place::pixel_to_addr;
+use crate::place::util::pixel_to_addr;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about)]
@@ -23,11 +20,10 @@ fn main() {
     let mut img = image::open(args.file).unwrap();
     img = img.resize_exact(512, 512, FilterType::Lanczos3);
 
-    let pixels = img.pixels().par_bridge().filter(|_px| /* cond */ true);
+    let pixels = img.pixels().filter(|_px| /* cond */ true);
     let addrs = pixels.map(|px| pixel_to_addr(px, 1));
 
     loop {
         addrs.clone().for_each(ping_v6);
-        std::thread::sleep(Duration::from_secs(5));
     }
 }
--- src/place.rs
+++ src/place.rs
@@ -1,35 +1,36 @@
-use std::{net::Ipv6Addr, str::FromStr};
+pub mod util {
+    use image::Rgba;
+    use std::{net::Ipv6Addr, str::FromStr};
 
-use image::Rgba;
+    const IP_FORMAT: &str = "2a06:a003:d040:SXXX:YYY:RR:GG:BB";
 
-const IP_FORMAT: &str = "2a06:a003:d040:SXXX:YYY:RR:GG:BB";
+    pub fn pixel_to_addr(px: (u32, u32, Rgba<u8>), size: usize) -> Ipv6Addr {
+        let x = px.0;
+        let y = px.1;
 
-pub fn pixel_to_addr(px: (u32, u32, Rgba<u8>), size: usize) -> Ipv6Addr {
-    let x = px.0;
-    let y = px.1;
+        let a = px.2[3];
+        let mut r = 0xFF;
+        let mut g = 0xFF;
+        let mut b = 0xFF;
 
-    let a = px.2[3];
-    let mut r = 0xFF;
-    let mut g = 0xFF;
-    let mut b = 0xFF;
+        if a != 0 {
+            r = px.2[0];
+            g = px.2[1];
+            b = px.2[2];
+        }
 
-    if a != 0 {
-        r = px.2[0];
-        g = px.2[1];
-        b = px.2[2];
+        if x > 512 || y > 512 {
+            panic!("Pixel out of bounds");
+        }
+
+        let addr_str = IP_FORMAT
+            .replace('S', &format!("{size:x}"))
+            .replace("XXX", &format!("{x:03x}"))
+            .replace("YYY", &format!("{y:03x}"))
+            .replace("RR", &format!("{r:02x}"))
+            .replace("GG", &format!("{g:02x}"))
+            .replace("BB", &format!("{b:02x}"));
+
+        Ipv6Addr::from_str(&addr_str).expect("formatted address should convert")
     }
-
-    if x > 512 || y > 512 {
-        panic!("Pixel out of bounds");
-    }
-
-    let addr_str = IP_FORMAT
-        .replace('S', &format!("{size:x}"))
-        .replace("XXX", &format!("{x:03x}"))
-        .replace("YYY", &format!("{y:03x}"))
-        .replace("RR", &format!("{r:02x}"))
-        .replace("GG", &format!("{g:02x}"))
-        .replace("BB", &format!("{b:02x}"));
-
-    Ipv6Addr::from_str(&addr_str).expect("formatted address should convert")
 }
